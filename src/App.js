3117d6c929fd9cb38e977e0a6b2d99b8bb6d8e4a
Adam Pasternack <adam.pasternack@gmail.com>
2017-07-09 13:14:47 -0400

    installed eslint rules and .selintrc file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Componenet } from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-class App extends Componenet {
+class App extends Component {
     render() {
         return (
             <Provider store={createStore(reducers)}>
