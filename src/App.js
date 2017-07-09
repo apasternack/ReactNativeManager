a7d6e0eaecd0ecb60eff8f2a1c02b2bb627538ab
Adam Pasternack <adam.pasternack@gmail.com>
2017-07-09 13:08:57 -0400

    pass in reducers to store

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Componenet } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 class App extends Componenet {
     render() {
         return (
-            <Provider store={createStore()}>
+            <Provider store={createStore(reducers)}>
                 <View>
                     <Text>
                         Hello!
