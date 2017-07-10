a083411b3b130ef75a4530ed3e24b9ef92365cee
Adam Pasternack <adam.pasternack@gmail.com>
2017-07-10 08:17:04 -0400

    firebase connection

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyDtIe9zRlAPDYZDHVIBK-hkhrKxZCiZ45A',
+            authDomain: 'manager-dc46a.firebaseapp.com',
+            databaseURL: 'https://manager-dc46a.firebaseio.com',
+            projectId: 'manager-dc46a',
+            storageBucket: 'manager-dc46a.appspot.com',
+            messagingSenderId: '166052078917'
+        };
+        firebase.initializeApp(config);
+    }
+    
+    
     render() {
         return (
             <Provider store={createStore(reducers)}>
