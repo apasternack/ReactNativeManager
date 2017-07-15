9072087dafdd7b80f1b03aebe9c9621f2ecd3ccf
Adam Pasternack <adam.pasternack@gmail.com>
2017-07-15 17:23:56 -0400

    return new immutable state, mapStateToProps, and pass state from redux to value of input

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,7 @@ const INITITAL_STATE = { email: '' };
 export default (state = INITITAL_STATE, action) => {
     switch (action.type) {
         case EMAIL_CHANGED:
-        
+            return { ...state, email: action.payload };
         
         default:
             return state;
